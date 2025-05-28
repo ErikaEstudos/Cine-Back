b6c07015434a48753ef4f37c06cd3dcdb7be5271
Erika <erikaestudosaraujo@gmail.com>
2025-05-28 19:45:38 -0300

    atualizando server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose"); //para conectar com MongoDB
 require("dotenv").config();
-//const path = require("path");
 
 const app = express(); //para criar o servidor
 app.use(cors()); //para permitir requisição do frontend
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "html"))); // Middleware para servir arquivos estáticos
 
 //conexão com MongoDB -- configuração
 const uri = process.env.MONGO_URI;
@@ -24,15 +22,6 @@ mongoose
 const usuarioRouter = require("./routes/usuarioRoutes");
 app.use("/usuarios", usuarioRouter);
 
-// Rota principal que carrega o HTML
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../html/index.html"));
-// });
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log("Servidor rodando...");
-// });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}...`);
